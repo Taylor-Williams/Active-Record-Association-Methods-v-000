529786437771b567cb007ac783194d1eba9387a5
Taylor Williams <taylor@thewilliams.net>
2018-08-02 01:19:38 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,6 +14,6 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    self.songs.map{|s|s.artist}.unique.map{|a|a.name}
+    self.songs.map{|s|s.artist}.uniq.map{|a|a.name}
   end
 end
